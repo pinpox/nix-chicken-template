956dff7f447f02a9e6d5e0c3927a465c3b8f9f12
Pablo Ovelleiro Corral <mail@pablo.tools>
2021-12-10 13:30:56 +0100

    delete stuff

--- flake.nix
+++ flake.nix
@@ -26,33 +26,38 @@
               name = "${pname}-${version}";
 
               src = self;
+
               # src = fetchegg {
               #   inherit version;
               #   name = pname;
               #   sha256 = "1l5zkr6b8l5dw9p5mimbva0ncqw1sbvp3d4cywm1hqx2m03a0f1n";
               # };
-              installPhase = ''
-                # mkdir -p $out
-                 runHook preInstall
-                 export CHICKEN_INSTALL_PREFIX=$out
-                 export CHICKEN_INSTALL_REPOSITORY=$out/lib/chicken/${
-                   toString chicken.binaryVersion
-                 }
-                echo THIS1
-                 chicken-install ${lib.concatStringsSep " " [ ]}
-                echo THIS
-                 ls
-                 for f in $out/bin/*
-                 do
-                   wrapProgram $f \
-                     --prefix CHICKEN_REPOSITORY_PATH : "$out/lib/chicken/${
-                       toString chicken.binaryVersion
-                     }:$CHICKEN_REPOSITORY_PATH" \
-                     --prefix CHICKEN_INCLUDE_PATH : "$CHICKEN_INCLUDE_PATH:$out/share" \
-                     --prefix PATH : "$out/bin:${chicken}/bin:$CHICKEN_REPOSITORY_PATH"
-                 done
-                 runHook postInstall
-              '';
+
+              # installPhase = ''
+              #   # mkdir -p $out
+              #    runHook preInstall
+              #    export CHICKEN_INSTALL_PREFIX=$out
+              #    export CHICKEN_INSTALL_REPOSITORY=$out/lib/chicken/${
+              #      toString chicken.binaryVersion
+              #    }
+
+              #    echo $CHICKEN_INSTALL_PREFIX
+              #    echo $CHICKEN_INSTALL_REPOSITORY
+              #   echo THIS1
+              #    chicken-install -v ${lib.concatStringsSep " " [ ]}
+              #   echo THIS
+              #    ls
+              #    for f in $out/bin/*
+              #    do
+              #      wrapProgram $f \
+              #        --prefix CHICKEN_REPOSITORY_PATH : "$out/lib/chicken/${
+              #          toString chicken.binaryVersion
+              #        }:$CHICKEN_REPOSITORY_PATH" \
+              #        --prefix CHICKEN_INCLUDE_PATH : "$CHICKEN_INCLUDE_PATH:$out/share" \
+              #        --prefix PATH : "$out/bin:${chicken}/bin:$CHICKEN_REPOSITORY_PATH"
+              #    done
+              #    runHook postInstall
+              # '';
 
               buildInputs = with eggs;
                 [
