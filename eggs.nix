c043585e86a9151716ac522b5a1faf9941bebada
Pablo Ovelleiro Corral <mail@pablo.tools>
2021-12-10 14:43:16 +0100

    make egg executable

--- eggs.nix
+++ eggs.nix
@@ -1,12 +1,16 @@
-{ eggDerivation, fetchegg }:
+{ pkgs, stdenv }:
+let
+  inherit (pkgs) eggDerivation fetchegg;
+in
 rec {
-  aes = eggDerivation {
-    name = "aes-1.5";
+
+  this = eggDerivation {
+    name = "this-0.1";
 
     src = fetchegg {
-      name = "aes";
-      version = "1.5";
-      sha256 = "0gjlvz5nk0fnaclljpyfk21rkf0nidjj6wcv3jbnpmfafgjny5fi";
+      name = "this";
+      version = "0.1";
+      sha256 = "1dz5lqfkyxbxr3s9806rc5n4kcg34wr1v83jjqq2p233w03j6j0f";
     };
 
     buildInputs = [
@@ -14,3 +18,4 @@ rec {
     ];
   };
 }
+
--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        eggs = with pkgs; import ./eggs.nix { inherit eggDerivation fetchegg; };
+        eggs = with pkgs; import ./eggs.nix { inherit pkgs stdenv; };
       in rec {
         packages = flake-utils.lib.flattenTree rec {
 
@@ -33,35 +33,10 @@
               #   sha256 = "1l5zkr6b8l5dw9p5mimbva0ncqw1sbvp3d4cywm1hqx2m03a0f1n";
               # };
 
-              # installPhase = ''
-              #   # mkdir -p $out
-              #    runHook preInstall
-              #    export CHICKEN_INSTALL_PREFIX=$out
-              #    export CHICKEN_INSTALL_REPOSITORY=$out/lib/chicken/${
-              #      toString chicken.binaryVersion
-              #    }
-
-              #    echo $CHICKEN_INSTALL_PREFIX
-              #    echo $CHICKEN_INSTALL_REPOSITORY
-              #   echo THIS1
-              #    chicken-install -v ${lib.concatStringsSep " " [ ]}
-              #   echo THIS
-              #    ls
-              #    for f in $out/bin/*
-              #    do
-              #      wrapProgram $f \
-              #        --prefix CHICKEN_REPOSITORY_PATH : "$out/lib/chicken/${
-              #          toString chicken.binaryVersion
-              #        }:$CHICKEN_REPOSITORY_PATH" \
-              #        --prefix CHICKEN_INCLUDE_PATH : "$CHICKEN_INCLUDE_PATH:$out/share" \
-              #        --prefix PATH : "$out/bin:${chicken}/bin:$CHICKEN_REPOSITORY_PATH"
-              #    done
-              #    runHook postInstall
-              # '';
-
               buildInputs = with eggs;
                 [
-                  # aes
+                  # Dependencies from eggs.nix
+                  this
                 ];
 
               meta = with lib; {
